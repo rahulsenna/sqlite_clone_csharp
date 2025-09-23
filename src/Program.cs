99033ee21a639fbd29d413e1354312662f49152f
Rahul Senna <rahulvsenna@gmail.com>
2025-09-23 12:12:22 +0530

    2: Print number of tables

--- src/Program.cs
+++ src/Program.cs
@@ -3,9 +3,9 @@ using static System.Buffers.Binary.BinaryPrimitives;
 // Parse arguments
 var (path, command) = args.Length switch
 {
-    0 => throw new InvalidOperationException("Missing <database path> and <command>"),
-    1 => throw new InvalidOperationException("Missing <command>"),
-    _ => (args[0], args[1])
+  0 => throw new InvalidOperationException("Missing <database path> and <command>"),
+  1 => throw new InvalidOperationException("Missing <command>"),
+  _ => (args[0], args[1])
 };
 
 var databaseFile = File.OpenRead(path);
@@ -13,15 +13,20 @@ var databaseFile = File.OpenRead(path);
 // Parse command and act accordingly
 if (command == ".dbinfo")
 {
-    Console.Error.WriteLine("Logs from your program will appear here!");
+  Console.Error.WriteLine("Logs from your program will appear here!");
 
-    databaseFile.Seek(16, SeekOrigin.Begin); // Skip the first 16 bytes
-    byte[] pageSizeBytes = new byte[2];
-    databaseFile.Read(pageSizeBytes, 0, 2);
-    var pageSize = ReadUInt16BigEndian(pageSizeBytes);
-    Console.WriteLine($"database page size: {pageSize}");
+  Console.WriteLine($"database page size: {ReadInt16(databaseFile, 16)}");
+  Console.WriteLine($"number of tables: {ReadInt16(databaseFile, 103)}");
 }
 else
 {
-    throw new InvalidOperationException($"Invalid command: {command}");
+  throw new InvalidOperationException($"Invalid command: {command}");
+}
+
+static int ReadInt16(FileStream bufStream, int offset)
+{
+  Span<byte> pageSizeBytes = stackalloc byte[2];
+  bufStream.Seek(offset, SeekOrigin.Begin);
+  bufStream.ReadExactly(pageSizeBytes);
+  return ReadUInt16BigEndian(pageSizeBytes);
 }
